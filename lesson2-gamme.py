c53411f961afc81323ac2e8b10d29684bed038b2
pro4ka <Erik.Polskoi@tptlive.ee>
2022-03-18 10:50:59 +0200

    Update lesson2-gamme.py

--- lesson2-gamme.py
+++ lesson2-gamme.py
@@ -18,62 +18,20 @@ start_position_x = 0
 start_positioin_y = 0
 current_position_x = start_position_x
 current_position_y = start_positioin_y
+def move(step, current_position_x, current_position_y):
+    print("current step is: ", step)
+    print("current_position_y =", current_position_y)
+    print("current_position_x = ", current_position_x)
+    can_move_right = current_position_x <= 2 and map [current_position_y][current_position_x+1]
+    can_move_bottom = current_position_y <= 2 and map [current_position_y+1][current_position_x]
+    if can_move_right:
+        print("Should move right")
+        current_position_x = current_position_x + 1
+    if can_move_bottom:
+        print("shoyld move down")
+        current_position_y = current_position_y + 1
+    return[current_position_x, current_position_y]
 
-print("current current_position_y = ", current_position_y)
-print("current current_position_x = ", current_position_x)
-can_move_right = map[current_position_y][current_position_x+1] == 0
-can_move_bottom = map[current_position_y+1][current_position_x] == 0
-if can_move_right:
-    print("should move right")
-if can_move_bottom:
-    print("Should move down")
-#2. step 2 
-can_move_right = map[current_position_y][current_position_x+1] == 0
-can_move_bottom = map[current_position_y+1][current_position_x] == 0
-
-if can_move_right:
-    print("should move right")
-    current_position_x = current_position_x + 1
-if can_move_bottom:
-    print("Should move down")
-    current_position_y = current_position_y + 1
-#3. step 3
-can_move_right = map[current_position_y][current_position_x+1] == 0
-can_move_bottom = map[current_position_y+1][current_position_x] == 0
-
-if can_move_right:
-    print("should move right")
-    current_position_x = current_position_x + 1
-if can_move_bottom:
-    print("Should move down")
-    current_position_y = current_position_y + 1
-#4. step 4 
-can_move_right = map[current_position_y][current_position_x+1] == 0
-can_move_bottom = map[current_position_y+1][current_position_x] == 0
-
-if can_move_right:
-    print("should move right")
-    current_position_x = current_position_x + 1
-if can_move_bottom:
-    print("Should move down")
-    current_position_y = current_position_y + 1
-#5. step 5 
-can_move_right = map[current_position_y][current_position_x+1] == 0
-can_move_bottom = map[current_position_y+1][current_position_x] == 0
-if can_move_right:
-    print("should move right")
-    current_position_x = current_position_x + 1
-if can_move_bottom:
-    print("Should move down")
-    current_position_y = current_position_y + 1
-#6. step 6
-can_move_right = map[current_position_y][current_position_x+1] == 0
-can_move_bottom = map[current_position_y+1][current_position_x] == 0
-
-if can_move_right:
-    print("should move right")
-    current_position_x = current_position_x + 1
-if can_move_bottom:
-    print("Should move down")
-    current_position_y = current_position_y + 1
-
+new_position = move (1, start_position_x, start_positioin_y)
+new_position = move (2, new_position[0], new_position[1])
+new_position = move (3, new_position[0], new_position[1])
